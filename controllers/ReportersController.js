ca78c1cda099824f1685d464c2d18cfeb420ef6e
Dex Fitch <fitch.dexter@gmail.com>
2015-02-27 13:49:01 -0800

    Complete logic for controllers, add HTML and directives.

--- controllers/ReportersController.js
+++ controllers/ReportersController.js
@@ -1,6 +1,19 @@
 beatDown.controller('ReportersCtrl', function ReportersCtrl($scope, $stateParams, BeatsFactory, UtilitiesFactory) {
   $scope.beat = UtilitiesFactory.findById(BeatsFactory.beats, $stateParams.beatId)
 
+  $scope.addReporter = function() {
 
+    $scope.beat.reporters.push({
+      firstName: $scope.firstName,
+      lastName: $scope.lastName,
+      dateEmployed: $scope.dateEmployed,
+      birthday: $scope.birthday
+    });
+
+    $scope.firstName = null;
+    $scope.lastName = null;
+    $scope.dateEmployed = null;
+    $scope.birthday = null;
+  };
 
  });
